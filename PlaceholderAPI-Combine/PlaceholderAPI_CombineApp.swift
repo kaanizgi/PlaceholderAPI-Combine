a3fe130c140e877e6416f88e66983621d820fdd1
Kaan İzgi <kaanizgi1531@gmail.com>
2022-08-18 15:42:52 +0300

    Initial Commit

---
+++ PlaceholderAPI-Combine/PlaceholderAPI_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceholderAPI_CombineApp.swift
+//  PlaceholderAPI-Combine
+//
+//  Created by Kaan İzgi on 18.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct PlaceholderAPI_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
